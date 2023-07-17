4028c34538ebe5ad50d9cd04c34fae3c5f27b9f1
Mostak1 <114609802+Mostak1@users.noreply.github.com>
2023-07-17 08:48:19 +0600

    'j'

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -32,6 +32,60 @@
             <div class="mx-auto text-center">Register to Get Our Service...</div>
             @if ($user)
             @endif
+
+
+        </div>
+    </div>
+    <div class="container my-5">
+        <!-- carousel starts here -->
+        <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="4000">
+                    <img src="{{ asset('assets/img/ca1.jpg') }}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="2000">
+                    <img src="{{ asset('assets/img/ca2.jpg') }}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('assets/img/ca3.jpg') }}" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+        <!-- --------------------------
+        ---------------End Carousel-------
+        -------------------------------- -->
+        <div class="my-3 row">
+            <div class="col-md-6">
+                <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_7WvWjFXF9K.json" background="transparent"
+                    speed="1" style="width: 400px;" loop autoplay></lottie-player>
+            </div>
+            <div class="col-md-6">
+                <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_bwnHjUCsaL.json" background="transparent"
+                    speed="1" style="width: 400px;" loop autoplay></lottie-player>
+            </div>
+        </div>
+        <!-- testpaper title dynamic -->
+        <div class="home_title">~Past Exam Papers~</div>
+        <div class="exam_data"></div>
+        <div class="my-3 row">
+            <div class="col-md-6">
+                <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_yg29hewu.json" background="transparent"
+                    speed="1" style="width: 600px; " loop autoplay></lottie-player>
+            </div>
+            <div class="col-md-6">
+                <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_1a8dx7zj.json" background="transparent"
+                    speed="1" style="width: 600px;" loop autoplay></lottie-player>
+            </div>
         </div>
     </div>
     <h3>Home Controler</h3>
