e714d91d65311db39173c25f0a0c8eda909209ed
unknown <mostakidb@gmail.com>
2023-08-01 15:19:58 +0600

    quizset

---
+++ app/Http/Controllers/QuizsetController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Quizset;
+use App\Http\Requests\StoreQuizsetRequest;
+use App\Http\Requests\UpdateQuizsetRequest;
+
+class QuizsetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreQuizsetRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Quizset $quizset)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Quizset $quizset)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateQuizsetRequest $request, Quizset $quizset)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Quizset $quizset)
+    {
+        //
+    }
+}
